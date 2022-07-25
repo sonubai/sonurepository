ceb80fcccbdc05036c62d1c72610e22fffd305ac
sonubai <sonubaipatil1234@gmail.com>
2022-07-25 14:58:26 +0530

    pass2

---
+++ src/main/java/Newpackage/Progect1.java
@@ -0,0 +1,19 @@
+package Newpackage;
+
+public class Progect1 {
+
+	public static void main(String[] args) {
+		System.out.println("priya");
+		System.out.println("amar");
+		System.out.println("poorna");
+		System.out.println("priya");
+		System.out.println("amar");
+		
+		
+		
+		
+		
+
+	}
+
+}
